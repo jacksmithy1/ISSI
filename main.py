fbed3559b7548978ab39a217c39b3af344e9426c
Lilli Nadol <lmn6@st-andrews.ac.uk>
2023-11-03 12:21:29 +0100

    sync changes

--- main.py
+++ main.py
@@ -14,12 +14,50 @@ import os
 # optimise choice of a and alpha, switch case for potential, LFF and MHS
 # automate nresol_z and zmax for generic data
 
+"""
+B0      : amplitude of magnetic field strength
+alpha   : linear force free parameter (in units of 1/L)
+a       : amplitude parameter of f(z)
+b       : parameter determining asymptotic value for z --> infinity
+z0      : z-value of "transition" in current density
+deltaz  : width of the transition in current density
+T0      : temperature at z=z0
+T1      : determines coronal temperature (T0+T1)
+H       : pressure scale height (measured in units of basic length) based on T0
+p0      : plasma pressure at z=0
+rho0    : plasma mass density at z=0
+nf_max  : number of Fourier modes in x and y
+kx      : wave numbers in the x-direction (2*pi*n/L_x)
+ky      : wave numbers in the y-direction (2*pi*m/L_y)
+k2      : array of kx^2 + ky^2 values
+p       : determined by k, a, b and alpha - sqrt(k^2*(1-a-a*b) - alpha^2)/2
+q       : determined by k, a, b and alpha - sqrt(k^2*(1-a+a*b) - alpha^2)/2
+anm     :
+bnm     :  array of Fourier coefficients of the sin(kx*x)*cos(ky*y) terms
+cnm     :  array of Fourier coefficients of the cos(kx*x)*sin(ky*y) terms
+dnm     :
+"""
 a = 0.0
 alpha = 0.0
     
 b = 1.0
-z0 = 0.4 # Where transition from NFF to FF takes place
-deltaz = z0/10.0 # Width of transitional region
+z0 = 0.4                    # Where transition from NFF to FF takes place
+deltaz = z0/10.0            # Width of transitional region
+
+T_photosphere = 5600.0      # temperature photosphere in Kelvin
+T_corona      = 2.0*10.0^6  # temperature corona in Kelvin
+
+T0 = (T_photosphere + T_corona*np.tanh(z0/deltaz))/(1.0+np.tanh(z0/deltaz))
+T1 = (T_corona - T_photosphere)/(1.0+np.tanh(z0/deltaz))
+
+g_solar = 274.0             # in m/s^2
+mbar =  1.0                 # photospheric mean molecular weight, as multiples of proton mass 
+H =  1.3807*T0/(mbar*1.6726*g_solar) *0.001    
+rho0  = 3.0-4               # photospheric mass density in kg/m^3
+B0    = 500.0               # normalising photospheric B-field in Gauss
+p0    = 1.3807*T_photosphere*rho0/(mbar*1.6726)*1.0     # photospheric plasma pressure in Pascal 
+pB0   = 3.9789-3*B0^2       # photospheric magnetic pressure in Pascal
+beta0 = p0/pB0              # photospheric plasma beta
 
 h1 = 0.0001 # Initial step length for fieldline3D
 eps = 1.0e-8 # Tolerance to which we require point on field line known for fieldline3D
