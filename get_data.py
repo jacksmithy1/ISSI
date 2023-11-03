e25b8fc3f7526cfc315f96975d19bbc136e1e376
Lilli Nadol <lmn6@st-andrews.ac.uk>
2023-11-03 20:53:37 +0100

    plot SOAR

--- get_data.py
+++ get_data.py
@@ -8,30 +8,31 @@ import math
 # Bx = np.array(ny, nx) and By = np.array(ny, nx) at z = 0 (Photosphere)
 # Additionally need pixelsize in km
 
+
 def get_magnetogram(path):
     data = scipy.io.readsav(path, python_dict=True, verbose=True)
 
-    #data_bz = data['b2dz5'] # [0:nresol_y,0:nresol_x]
-    # Y-axis size first as this corresponds to number of rows, then X-Axis size corresponding t number of columns  
-    #data_bx = data['b2dx5'] # [0:nresol_y,0:nresol_x]
+    # data_bz = data['b2dz5'] # [0:nresol_y,0:nresol_x]
     # Y-axis size first as this corresponds to number of rows, then X-Axis size corresponding t number of columns
-    #data_by = data['b2dy5'] # [0:nresol_y,0:nresol_x]
+    # data_bx = data['b2dx5'] # [0:nresol_y,0:nresol_x]
     # Y-axis size first as this corresponds to number of rows, then X-Axis size corresponding t number of columns
-    #scale_height = data['h3d']
+    # data_by = data['b2dy5'] # [0:nresol_y,0:nresol_x]
+    # Y-axis size first as this corresponds to number of rows, then X-Axis size corresponding t number of columns
+    # scale_height = data['h3d']
 
-    data_bz = data['b2dz'] # [0:nresol_y,0:nresol_x]
-    # Y-axis size first as this corresponds to number of rows, then X-Axis size corresponding t number of columns  
-    data_bx = data['b2dx'] # [0:nresol_y,0:nresol_x]
+    data_bz = data["b2dz"]  # [0:nresol_y,0:nresol_x]
     # Y-axis size first as this corresponds to number of rows, then X-Axis size corresponding t number of columns
-    data_by = data['b2dy'] # [0:nresol_y,0:nresol_x]
+    data_bx = data["b2dx"]  # [0:nresol_y,0:nresol_x]
+    # Y-axis size first as this corresponds to number of rows, then X-Axis size corresponding t number of columns
+    data_by = data["b2dy"]  # [0:nresol_y,0:nresol_x]
     # Y-axis size first as this corresponds to number of rows, then X-Axis size corresponding t number of columns
 
-    print(data['info_unit'])
-    print(data['info_pixel'])
-    #print(data['info_boundary'])
-    print(data['info_array'])
+    print(data["info_unit"])
+    print(data["info_pixel"])
+    # print(data['info_boundary'])
+    print(data["info_array"])
 
-    pixelsize = float(input('Pixelsize in km?'))
+    pixelsize = float(input("Pixelsize in km?"))
 
     bx_xlen = data_bx.shape[1]
     bx_ylen = data_bx.shape[0]
@@ -40,56 +41,75 @@ def get_magnetogram(path):
     bz_xlen = data_bz.shape[1]
     bz_ylen = data_bz.shape[0]
 
-    if (bx_xlen != by_xlen or bx_xlen != bz_xlen or by_xlen != bz_xlen): 
-        print('x lengths of data do not match') 
+    if bx_xlen != by_xlen or bx_xlen != bz_xlen or by_xlen != bz_xlen:
+        print("x lengths of data do not match")
         raise ValueError
-    if (bx_ylen != by_ylen or bx_ylen != bz_ylen or by_ylen != bz_ylen): 
-        print('y lengths of data do not match') 
+    if bx_ylen != by_ylen or bx_ylen != bz_ylen or by_ylen != bz_ylen:
+        print("y lengths of data do not match")
         raise ValueError
     else:
-        nresol_x = bx_xlen # Data resolution in x direction
-        nresol_y = bx_ylen # Data resolution in y direction
+        nresol_x = bx_xlen  # Data resolution in x direction
+        nresol_y = bx_ylen  # Data resolution in y direction
 
     L = 1.0
 
-    xmin = 0.0 # Minimum value of x in data length scale, not in Mm
-    ymin = 0.0 # Minimum value of y in data length scale, not in Mm
-    zmin = 0.0 # Minimum value of z in data length scale, not in Mm
+    xmin = 0.0  # Minimum value of x in data length scale, not in Mm
+    ymin = 0.0  # Minimum value of y in data length scale, not in Mm
+    zmin = 0.0  # Minimum value of z in data length scale, not in Mm
 
     if nresol_x < nresol_y:
-        xmax = L # Maximum value of x in data length scale, not in Mm
-        ymax = nresol_y/nresol_x # Maximum value of y in data length scale, not in Mm
+        xmax = L  # Maximum value of x in data length scale, not in Mm
+        ymax = nresol_y / nresol_x  # Maximum value of y in data length scale, not in Mm
     if nresol_y < nresol_x:
         ymax = L
-        xmax = nresol_x/nresol_y
+        xmax = nresol_x / nresol_y
     if nresol_y == nresol_x:
         xmax = L
         ymax = L
-  
-    pixelsize_x = abs(xmax-xmin)/nresol_x # Data pixel size in x direction
-    pixelsize_y = abs(ymax-ymin)/nresol_y # Data pixel size in y direction
-    
+
+    pixelsize_x = abs(xmax - xmin) / nresol_x  # Data pixel size in x direction
+    pixelsize_y = abs(ymax - ymin) / nresol_y  # Data pixel size in y direction
+
     if pixelsize_x != pixelsize_y:
-        print('directional pixel sizes of data do not match') 
+        print("directional pixel sizes of data do not match")
         raise ValueError
-    
-    nresol_z = math.floor(10000.0/pixelsize) # Artifical upper boundary at 10Mm outside of corona
-    z0_index = math.floor(2000.0/pixelsize)  # Height of Transition Region at 2Mm
+
+    nresol_z = math.floor(
+        10000.0 / pixelsize
+    )  # Artifical upper boundary at 10Mm outside of corona
+    z0_index = math.floor(2000.0 / pixelsize)  # Height of Transition Region at 2Mm
 
     if xmax == L:
-        zmax = nresol_z/nresol_x
-        z0 = z0_index/nresol_x
+        zmax = nresol_z / nresol_x
+        z0 = z0_index / nresol_x
     if ymax == L:
-        zmax = nresol_z/nresol_y
-        z0 = z0_index/nresol_y
+        zmax = nresol_z / nresol_y
+        z0 = z0_index / nresol_y
 
-    pixelsize_z = abs(zmax-zmin)/nresol_z # Data pixel size in z direction
+    pixelsize_z = abs(zmax - zmin) / nresol_z  # Data pixel size in z direction
 
     if pixelsize_z != pixelsize_x:
-        print('nresol_z and zmax do not match') 
+        print("nresol_z and zmax do not match")
         raise ValueError
-    
-    nf_max = min(nresol_x, nresol_y)-1
 
-    return [data_bx, data_by, data_bz, nresol_x, nresol_y, nresol_z, pixelsize_x, pixelsize_y, pixelsize_z, nf_max, xmin, xmax, ymin, ymax, zmin, zmax, z0]
+    nf_max = min(nresol_x, nresol_y) - 1
 
+    return [
+        data_bx,
+        data_by,
+        data_bz,
+        nresol_x,
+        nresol_y,
+        nresol_z,
+        pixelsize_x,
+        pixelsize_y,
+        pixelsize_z,
+        nf_max,
+        xmin,
+        xmax,
+        ymin,
+        ymax,
+        zmin,
+        zmax,
+        z0,
+    ]
--- read_SOARdata.py
+++ read_SOARdata.py
@@ -4,23 +4,24 @@ import sunpy_soar
 from sunpy.net import Fido, attrs as a
 from astropy.time import Time
 import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import numpy as np
+import plot_magnetogram
+
+
+# TO DO
+# Extract boundary magnetic field vector from Magnetogram
+# Gar nicht mal so einfach
 
 # Magnetic field B Line Of Sight character
 path_blos = (
     "/Users/lilli/Desktop/SOAR/obs/solo_L2_phi-hrt-blos_20220307T000609_V01.fits"
 )
 
-
 with fits.open(path_blos) as data:
     data.info()
     image_data = fits.getdata(path_blos, ext=0)
-    print(image_data.shape)
-    plt.figure()
+    # print(image_data.shape)
     photo_data = image_data[500:1000, 400:1200]
-    plt.imshow(photo_data, cmap="bone")
-    plt.colorbar()
-    plt.show()
-    # info = fits.getdata(path_blos, ext=10)
 
-print(photo_data.shape)
-# print(info)
+plot_magnetogram.plot_magnetogram_boundary(photo_data, 800, 500)
